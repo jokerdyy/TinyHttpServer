4b58930f7ed560f5df0555961866e32267966a9e
jokerdyy <1158447564@qq.com>
2022-07-19 17:03:24 +0800

    v2.0

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TinyHttpServer
 
 http_server_onethread文件夹内包含了windows下，单线程的简易http服务器实现。
+
+开发环境：win10+vs2019
---
+++ TinyHttpServer/include/noncopyable.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//#ifndef WEBSERVER_NONCOPYABLE_H
+//#define WEBSERVER_NONCOPYABLE_H
+
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+
+protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+
+};
+//#endif //WEBSERVER_NONCOPYABLE_H
